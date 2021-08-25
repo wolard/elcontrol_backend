55f95a972c9cd747a7270e57a6740a6590957276
klaus <you@example.com>
2021-08-26 00:09:37 +0300

    branch

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ var express = require("express");
 var ModbusRTU = require("modbus-serial");
 var client = new ModbusRTU();
 var app = express();
-client.connectRTUBuffered("/dev/serial0", { baudRate: 9600 });
+var fs = require('fs');
+let configjson;
+configjson=fs.readFileSync('config.json').toString();
+console.log(configjson);
+let ConfObj=JSON.parse(configjson);
+//client.connectRTUBuffered("/dev/serial0", { baudRate: 9600 });
 
-const ledoff = () =>client.writeFC6(1,5,512);
-const ledon = () =>client.writeFC6(1,5,256);
+//const ledoff = () =>client.writeFC6(1,5,512);
+//const ledon = () =>client.writeFC6(1,5,256);
 app.get("/ledon", (req, res, next) => {
-    ledon();
+  //  ledon();
     res.json({"ledi":"päällä"});
    });
    app.get("/ledoff", (req, res, next) => {
-    ledoff();
+   // ledoff();
     res.json({"ledi":"pois"});
    });
 app.listen(3000, () => {
