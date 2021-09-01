e6a67682cebff234bf2e3b84544a081816e16256
Klaus Sandell <wolard@gmail.com>
2021-09-01 19:13:21 +0100

    added websocket

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
-var express = require("express");
-var cors = require('cors')
-var ModbusRTU = require("modbus-serial");
-var client = new ModbusRTU();
-var app = express();
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const cors = require('cors')
+const ModbusRTU = require("modbus-serial");
+var gpio = require('rpi-gpio');
+var val;
+var oldval;
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+server.listen(8888);
+
+//var client = new ModbusRTU();
+//const app = express();
 app.use(cors());
 app.use(express.json());
 var fs = require('fs');
-
+//client.connectRTUBuffered("/dev/serial0", { baudRate: 9600 });
 let configjson;
+var client = new ModbusRTU();
+client.connectRTUBuffered("/dev/serial0", { baudRate: 9600 });
+let interval;
+
+const getApiAndEmit = socket => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  io.on("connection", (socket) => {
+    console.log("New client connected");
+    socket.emit("sdiofjsdiofj");
+    socket.on("disconnect", () => {
+      console.log("Client disconnected");
+ 
+    });
+  });
+
+};
+var timeoutObj = setTimeout(() => {
+  console.log('timeout beyond time');
+}, 200);
+
+gpio.setup(16, gpio.DIR_IN, gpio.EDGE_FALLING);
+gpio.on('change', function(channel, value) {
+
+
+	clearTimeout(timeoutObj);
+  timeoutObj = setTimeout(() => {
+    io.emit("FromAPI", value);
+  }, 200);
+
+   
+  
+  });
+
+//});
 
 
 configjson=fs.readFileSync('config.json').toString();
@@ -15,19 +65,24 @@ console.log(configjson);
 const ConfObj=JSON.parse(configjson);
 
 const modbushandler =(command) => {
-client.writefc6(command.card,command.relay,1280);
+
+client.writeFC6(command.card,command.relay,1280);
 
 }
 //client.connectRTUBuffered("/dev/serial0", { baudRate: 9600 });
 
 //const ledoff = () =>client.writeFC6(1,5,512);
-//const ledon = () =>client.writeFC6(1,5,256);
-app.post("/light", (req, res, next) => {
-   console.log(req.body);
-   let command=req.body;
-modbushandler(command);
 
-   res.sendStatus(200);
+app.post("/light", (req, res, next) => {
+  // console.log(req.body);
+   let command=req.body;
+   modbushandler(command);
+  
+    res.sendStatus(200);
+//
+
+
+  
     
  });
 app.get("/init", (req, res, next) => {
